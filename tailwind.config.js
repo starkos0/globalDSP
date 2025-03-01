41270b2e67772287c720e029f5ab13df9a0bcbab
starkos0 <alexbadoiu50@gmail.com>
2025-03-01 17:42:59 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,40 +12,5 @@ module.exports = {
         DEFAULT: '0.125rem', // Cambia el valor predeterminado a rounded-sm
       },
     },
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: [
-      {
-        dark: {
-          primary: '#505050',
-          'primary-focus': '#4506cb',
-          'primary-content': '#ffffff',
-          secondary: '#202020',
-          'secondary-focus': '#bd0091',
-          'secondary-content': '#ffffff',
-          accent: '#37cdbe',
-          'accent-focus': '#2aa79b',
-          'accent-content': '#ffffff',
-          neutral: '#3d4451',
-          'neutral-focus': '#2a2e37',
-          'neutral-content': '#ffffff',
-          'base-100': '#131313',
-          'base-200': '#272a30',
-          'base-300': '#080807',
-          'base-content': '#E1D5CD',
-          info: '#2094f3',
-          success: '#009485',
-          warning: '#ff9900',
-          error: '#ff5724',
-          fontFamily: {
-            body: ['Poppins', 'sans-serif'],
-            display: ['Satoshi', 'sans-serif'],
-          },
-        },
-      },
-      // otros temas que desees mantener
-      'light',
-    ],
-  },
+  }
 };
